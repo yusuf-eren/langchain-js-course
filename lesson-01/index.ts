af3a06ff7f3298f28619c398658ea27348eca866
Yusuf Eren <erenyusuf170@gmail.com>
2025-08-16 00:28:20 +0300

    feat: LangChain.js Course intro-01

---
+++ lesson-01/index.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { ChatOpenAI } from '@langchain/openai';
+import { HumanMessage, SystemMessage } from '@langchain/core/messages';
+
+const model = new ChatOpenAI({ model: 'gpt-4o' });
+
+const messages = [
+  new SystemMessage('You are a helpful assistant.'),
+  new HumanMessage('What is the capital of France?'),
+  { role: 'user', content: 'za' },
+];
+
+async function main() {
+  const result = await model.invoke(messages);
+  console.log(result.content);
+  messages.push(result);
+  messages.push(new HumanMessage('What I asked to you?'));
+  const result2 = await model.invoke(messages);
+  console.log(result2.content);
+}
+
+main();
